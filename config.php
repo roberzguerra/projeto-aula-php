e670287ac2c716bb93e54d7f3ea7ca734b73553c
Rober Guerra <roberzguerra@gmail.com>
2018-07-11 19:12:09 -0300

    commit do arquivo config.php

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+// Inclui arquivo de configuração e metodos de conexão com banco de dados.
+include 'db.php';
+
+// Inclui arquivo util.php contendo algumas funções de uso comum do sistema.
+include 'utils.php';
+
+/**
+ * Arquivo de configuração do projeto
+ */
+
+
+// Ativar exibição de erros no PHP */
+ini_set('display_errors', 1);
+ini_set('display_startup_erros', 1);
+error_reporting(E_ALL);
+
+$SITE_URL = 'http://localhost/cadastro-pessoa';
+?>
+
+
+
+
