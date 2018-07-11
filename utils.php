92d3d4d4a660059bfedbfed67a4b1bad8e1ad663
Rober Guerra <roberzguerra@gmail.com>
2018-07-11 19:54:15 -0300

    Alterado utils.php

--- utils.php
+++ utils.php
@@ -49,9 +49,18 @@ function validarEmail($email) {
     return false;
 }
 
-/*
-Testes com github
-*/
 
+
+function teste($email) {
+    $conta = "/^[a-zA-Z0-9\._-]+@";
+    $domino = "[a-zA-Z0-9\._-]+.";
+    $extensao = "([a-zA-Z]{2,4})$/";
+    $pattern = $conta.$domino.$extensao;
+
+    if (preg_match($pattern, $email)) {
+        return true;
+    }
+    return false;
+}
 
 ?>
