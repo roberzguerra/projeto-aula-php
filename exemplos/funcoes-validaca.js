e2cd3b5519182c390d6fcb53604cadbc87740a4a
Rober Guerra <roberzguerra@gmail.com>
2018-08-08 21:14:57 -0300

    Funções de validação adicionadas no diretório de exemplos.

---
+++ exemplos/funcoes-validaca.js
@@ -0,0 +1,32 @@
+function validarCpf(strCPF) {
+    var Soma;
+    var Resto;
+    Soma = 0;
+    if (strCPF == "00000000000") return false;
+
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
+
+    if ((Resto == 10) || (Resto == 11))  Resto = 0;
+    if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+    return true;
+}
+
+function removerMascaraCpf(cpf) {
+    return cpf.replace(/[^\d]+/g,'');
+}
+
+function validarEmail(valor) {
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+    if (testEmail.test(valor)) {
+        return true;
+    }
+    return false;
+}
